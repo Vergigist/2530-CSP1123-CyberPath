d7d3f6976806f417fb13e5f84782a807f9b7390a
jackho06 <hozhenxiang@gmail.com>
2025-11-06 13:36:09 +0800

    Finished database and admin login/sign up (not tested)

---
+++ app/admin.py
@@ -0,0 +1,44 @@
+# from flask import Blueprint, render_template, request, redirect, url_for, flash
+# from . import db
+# from .models import User
+
+
+# bp = Blueprint("admin", __name__, url_prefix="/admin")
+
+
+# @bp.route("/signup", methods=["GET", "POST"])
+
+# def signup():
+#     if request.method == "POST":
+#         username = request.form["username"]
+#         password = request.form["password"]
+
+#         # Checking if admin already exist
+#         existing = User.query.filter_by(username=username).first()
+#         if existing:
+#             flash("Username already exists.")
+#             return redirect(url_for("admin.signup"))
+
+#         # Creating new admin
+#         new_user = User(username=username, password=password)
+#         db.session.add(new_user)
+#         db.session.commit()
+#         flash("Account created successfully!")
+#         return redirect(url_for("admin.login"))
+
+#     return render_template("signup.html")
+
+
+# @bp.route("/login", methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         username = request.form["username"]
+#         password = request.form["password"]
+
+#         user = User.query.filter_by(username=username, password=password).first()
+#         if user:
+#             flash("Login successful!")
+#             return redirect(url_for("main.index"))
+#         else:
+#             flash("Invalid username or password.")
+#     return render_template("login.html")
