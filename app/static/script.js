d826b2e4bbd8c6eec2d96afce37e2e27c8cec8bc
Joshua <joshuatengyj@gmail.com>
2025-11-07 20:23:47 +0800

    Admin sign in/sign up & Display map coordinates

--- app/static/script.js
+++ app/static/script.js
@@ -26,12 +26,43 @@ toggleBtn.addEventListener('click', () => {
     }, 310);
 });
 
-// Center button
 centerBtn.addEventListener('click', () => {
     map.setView([2.928, 101.64192], 16);
 });
 
-// Ensure map resizes properly after sidebar animation
 sidebar.addEventListener('transitionend', () => {
     map.invalidateSize();
+});
+
+
+// Admin 
+const adminBtn = document.getElementById('adminBtn');
+const adminPopup = document.getElementById('adminPopup');
+const closePopup = document.getElementById('closePopup');
+
+adminBtn.addEventListener('click', () => adminPopup.style.display = 'flex');
+closePopup.addEventListener('click', () => adminPopup.style.display = 'none');
+window.addEventListener('click', (e) => { if(e.target === adminPopup) adminPopup.style.display = 'none'; });
+
+const tabs = document.querySelectorAll('.tab-btn');
+const forms = document.querySelectorAll('.tab-form');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        forms.forEach(f => f.classList.remove('active'));
+
+        tab.classList.add('active');
+        document.getElementById(tab.dataset.tab + 'Form').classList.add('active');
+    });
+});
+
+//Map get coordinates interaction
+
+const coordsDisplay = document.getElementById('coords-display');
+
+map.on('click', function(e) {
+    const lat = e.latlng.lat.toFixed(6);
+    const lng = e.latlng.lng.toFixed(6);
+    coordsDisplay.textContent = `Latitude: ${lat}, Longitude: ${lng}`;
 });
